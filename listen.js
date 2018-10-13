4bd77a0c0338544e906beea4174c4f69dd15e14a
tommyrawle <tommyrawle@hotmail.com>
2018-10-13 19:31:54 +0100

    FINAL commit

--- listen.js
+++ listen.js
@@ -1,6 +1,7 @@
+let port = process.env.PORT || 9090
+
 const app = require('./app');
 
-
-app.listen(9090, () => {
-    console.log('listening on port 9090...')
+app.listen(port, () => {
+    console.log('listening...')
 })
