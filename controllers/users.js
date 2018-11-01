87a76ac3d5f5083b13e78fd085368963cb775d4c
tommyrawle <tommyrawle@hotmail.com>
2018-11-01 21:46:12 +0000

    added more user routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const {User} = require('../models')
+const {User, Comment, Article} = require('../models')
 
 exports.getUserProfile = (req, res, next) => {
     const {username} = req.params
@@ -9,4 +9,32 @@ exports.getUserProfile = (req, res, next) => {
             res.status(200).send({userProfile})
         })
         .catch(next)
+}
+
+exports.getCommentsByUserId = (req, res, next) => {
+    const {user_id} = req.params
+    Comment.find({'created_by': user_id})
+    .populate('belongs_to')
+    .then(comments => {
+        if (!comments.length) return Promise.reject({status: 404, msg: 'no comments found'})
+        res.status(200).send({comments})
+    })
+    .catch(err => {
+        if (err.name === 'CastError') next({status: 400, msg: 'invalid user id'})
+        else next(err);
+    })
+}
+
+exports.getArticlesByUserId = (req, res, next) => {
+    const {user_id} = req.params
+    Article.find({'created_by': user_id})
+    .populate('created_by')
+    .then(articles => {
+        if (!articles.length) return Promise.reject({status: 404, msg: 'no articles found'})
+        res.status(200).send({articles})
+    })
+    .catch(err => {
+        if (err.name === 'CastError') next({status: 400, msg: 'invalid user id'})
+        else next(err);
+    })
 }
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const {getUserProfile} = require('../controllers/users')
+const {getUserProfile, getCommentsByUserId, getArticlesByUserId} = require('../controllers/users')
 
 router.route('/:username')
     .get(getUserProfile)
-
-    
+router.route('/:user_id/comments')
+    .get(getCommentsByUserId)
+router.route('/:user_id/articles')
+    .get(getArticlesByUserId)
 
 
 module.exports = router
